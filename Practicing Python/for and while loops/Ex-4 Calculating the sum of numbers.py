eb00f3330c0c484d9b55e57ef7afea406d393f2f
BGanade <bganade@gmail.com>
2025-05-20 16:02:41 -0300

    Ex-6 for and while loops

--- Practicing Python/for and while loops/Ex-4 Calculating the sum of numbers.py
+++ Practicing Python/for and while loops/Ex-4 Calculating the sum of numbers.py
@@ -1,7 +1,9 @@
+# Calculate the total sum of numbers
+
 numbers = [10, 20, 30, 40, 50]
-sum = 0
 
+total_sum = 0
 for number in numbers:
-    sum = sum + number
+    total_sum += number
 
-print(sum)
+print(f"The total sum of revenues is: {total_sum}")
---
+++ Practicing Python/for and while loops/Ex-6 Understanding the use of break.py
@@ -0,0 +1,7 @@
+books = ["1984", "Dom Casmurro", "O Pequeno Príncipe",
+         "O Hobbit", "Orgulho e Preconceito"]
+
+for book in books:
+    if book == "O Hobbit":
+        print(f"book found {book}")
+        break
