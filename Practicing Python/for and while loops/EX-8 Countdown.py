18fc8f383bf9cd1cedab99f21848f266dd12d9e4
BGanade <bganade@gmail.com>
2025-05-21 16:12:03 -0300

    Ex-8 for and while loops

---
+++ Practicing Python/for and while loops/EX-8 Countdown.py
@@ -0,0 +1,12 @@
+# Loop while time is greater than 0
+for seconds in range(10, 0, -1):
+    # Check if the current time is an even number
+    if seconds % 2 == 0:
+        # Print a specific message for even numbers
+        print(f"Only {seconds} seconds left - Don't miss this opportunity!")
+    else:
+        # Print a different message for odd numbers
+        print(f"The countdown continues: {seconds} seconds remaining.")
+
+    # Decrement the time by 1 in each iteration
+    seconds -= 1
