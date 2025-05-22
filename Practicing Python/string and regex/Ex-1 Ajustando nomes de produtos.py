d2298d6f1bcb4c5cc9526ce3d31c5a8924f07da8
BGanade <bganade@gmail.com>
2025-05-22 17:12:50 -0300

    Ex-1 string and regex fixed

--- Practicing Python/string and regex/Ex-1 Ajustando nomes de produtos.py
+++ Practicing Python/string and regex/Ex-1 Ajustando nomes de produtos.py
@@ -1,3 +1,8 @@
+# Get product name from user
 product_name = input("Type the name of the product: ")
-product_name = product_name.lower()
-print(product_name)
+
+# Convert the product name to lowercase for standardization
+product_standardized = product_name.lower()
+
+# Print the standardized product name
+print(product_standardized)
